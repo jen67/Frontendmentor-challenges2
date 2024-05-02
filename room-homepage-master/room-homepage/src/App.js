2d2c180ddad8663aa0d0d2a347e1f08e070fe59b
jen67 <amakrigift2000@gmail.com>
2024-05-02 19:01:19 +0100

    added more codes

--- room-homepage-master/room-homepage/src/App.js
+++ room-homepage-master/room-homepage/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './Footer/Footer'; 
 import Hero from './Hero/Hero';
+import SectionB from './Section/SectionB';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <SectionB />
       <Footer />
     </div>
   );
---
+++ room-homepage-master/room-homepage/src/Section/SectionB.jsx
@@ -0,0 +1,24 @@
+import "./SectionB.css";
+
+function SectionB() {
+  return (
+    <section className="section-b">
+      <div className="first-Img"></div>
+      <article>
+        <h2> About our furniture</h2>
+        <p>
+          Our multifunctional collection blends design and function to suit your
+          individual taste. Make each room unique, or pick a cohesive theme that
+          best express your interests and what inspires you. Find the furniture
+          pieces you need, from traditional to contemporary styles or anything
+          in between. Product specialists are available to help you create your
+          dream space.
+        </p>
+      </article>
+
+      <div className="second-Img"></div>
+    </section>
+  );
+}
+
+export default SectionB;
