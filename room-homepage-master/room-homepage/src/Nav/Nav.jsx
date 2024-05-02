9b19fc30f7ece3761e021d2e3c342e06afcfc9e7
jen67 <amakrigift2000@gmail.com>
2024-05-02 19:16:43 +0100

    more codes

--- room-homepage-master/room-homepage/src/Nav/Nav.jsx
+++ room-homepage-master/room-homepage/src/Nav/Nav.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import logo from "../images/logo.svg";
+import handburger from "../images/icon-hamburger.svg";
+import close from "../images/icon-close.svg";
 
 function Nav() {
   return (
     <header>
       <nav>
-        <img src={logo} alt="Room logo" /> 
+        <div>
+          <img src={logo} alt="Room logo" />{" "}
+        </div>
+        <div className="menu-icons">
+          <img src={handburger} alt="menu icon" />
+          <img src={close} alt="close icon" />
+        </div>
+
         <ul>
-          <li><a href="#/">Home</a></li>
-          <li><a href="#/">Shop</a></li>
-          <li><a href="#/">About</a></li>
-          <li><a href="#/">Contact</a></li>
+          <li>
+            <a href="#/">Home</a>
+          </li>
+          <li>
+            <a href="#/">Shop</a>
+          </li>
+          <li>
+            <a href="#/">About</a>
+          </li>
+          <li>
+            <a href="#/">Contact</a>
+          </li>
         </ul>
       </nav>
     </header>
