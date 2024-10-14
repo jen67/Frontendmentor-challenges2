eebd5bbe9ff8b8990e08ed3389cda9f7770f9c63
jen67 <amakrigift2000@gmail.com>
2024-10-14 15:08:23 +0100

    added a new challenge

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -1,4 +1,28 @@
-fetch("./data.json")
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error('Error:', error));
+async function fetchData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+function createChart(ctx, data) {
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: data.map((item) => item.day),
+      datasets: [
+        {
+          label: "Custom Label",
+          data: data.map((item) => item.amount),
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+}
+
+fetchData("./data.json").then((data) => {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  createChart(ctx, data);
+});
