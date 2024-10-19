be70bf4b976792328307333cfbfd6ec5ce34b9db
jen67 <amakrigift2000@gmail.com>
2024-10-19 22:08:56 +0100

    added more functionalities

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -1,23 +1,38 @@
 (function () {
-    "use strict";
-    
-    document.addEventListener("DOMContentLoaded", () => {
-        
+  "use strict";
 
+  document.addEventListener("DOMContentLoaded", () => {
+    const body = document.body;
+    const themeToggler = document.querySelector("#theme-switcher");
+    const header = document.querySelector("header");
+    const Form = document.querySelector("#task-input-form");
+    const inputForm = document.querySelector("#task-input");
+    const TaskContainer = document.querySelector(".todo-list-container");
 
-
-    })
+      
+    function applytheme(theme) {
+      if (theme === "dark") {
+          body.classList.add("dark-theme");
+          themeToggler.src = "./images/icon-sun.svg";
+          themeToggler.alt = "sun icon";
+        }
+      else {
+          body.classList.remove('dark-theme');
+          themeToggler.src = "./images/icon-moon.svg";
+          themeToggler.alt = "moon icon";
+        }
+    }
+  });
 })();
 
-const themeToggler = document.querySelector("#theme-switcher");
 const circledCheck = document.querySelectorAll(".circled-check");
 const listItems = document.querySelectorAll(".todo-item");
 const deleteItems = document.getElementsByClassName("delete-task");
 const body = document.body;
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
 // Add event listener to each circledCheck element
-circledCheck.forEach(check => {
+circledCheck.forEach((check) => {
   check.addEventListener("click", () => {
     // Add your click event logic here
   });
@@ -27,12 +42,9 @@ themeToggler.addEventListener("click", () => {
   body.classList.toggle("dark-theme");
 
   if (body.classList.contains("dark-theme")) {
-    themeToggler.src = "./images/icon-sun.svg";
-      themeToggler.alt = "sun icon";
-      header.style.backgroundImage = "url(./images/bg-mobile-dark.jpg)"
+    
   } else {
     themeToggler.src = "./images/icon-moon.svg";
-      themeToggler.alt = "moon icon";
-      header.style.backgroundImage = "url(./images/bg-mobile-light.jpg)";
+    themeToggler.alt = "moon icon";
   }
 });
